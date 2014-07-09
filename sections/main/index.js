7e40f7cfbc5059ab7a55828e456788a8b7127118
diego <diego.lopez.arcos@gmail.com>
2014-07-09 18:09:33 -0500

    :) verify the users online, ok

--- sections/main/index.js
+++ sections/main/index.js
@@ -3,13 +3,44 @@
 
 module.exports = function ( params ) {
   var io = params.io,
-      server = params.server;
+      server = params.server,
+      users = [];
 
   io.on('connection', function ( socket ) {
     console.log('Conexion');
+
     socket.on('send message', function ( data ) {
       console.log(data);
       socket.broadcast.emit('new message', data);
+      socket.emit('new message', data);
+    });
+
+
+    socket.on('remove user', function ( data ) {
+      var message = {
+        username: 'system',
+        date: new Date(),
+        message: data.username + "is offline!",
+        type: 'label'
+      };
+      socket.broadcast.emit('remove user', message);
+      var index = users.indexOf(data);
+      if (index > -1) {
+          users.splice(index, 1);
+      }
+    });
+
+    socket.on('add user', function ( data ) {
+      console.log(data);
+      users.push(data);
+      var message = {
+        username: 'system',
+        date: new Date(),
+        message: data.username + "is on!.",
+        type: 'label'
+      };
+      socket.broadcast.emit('new user', message);
+      socket.emit('online users', users);
     });
   });
 };
--- sections/main/main.controller.browser.js
+++ sections/main/main.controller.browser.js
@@ -8,20 +8,61 @@ module.exports = function ( angular, app ) {
   main.$inject = [ '$scope', 'Socket' ];
   function main ( scope, Socket ) {
     scope.messages = [];
+    scope.users = [];
 
     Socket.on( 'new message', function ( data ) {
-      console.log(data);
       scope.messages.push(data);
     });
 
+    Socket.on( 'new user', function ( data ) {
+      scope.messages.push(data);
+    });
+
+    Socket.on( 'remove user', function ( data ) {
+      scope.messages.push(data);
+    });
+
+    Socket.on( 'online users', function ( data ) {
+      console.log(data.length);
+      var message = {
+        username: '',
+        date: '',
+        message: "Hay " + data.length + " conectados.",
+        type: 'label [round radius]'
+      };
+      scope.messages.push(message);
+      scope.users = data;
+    });
+
+    scope.addUser = function () {
+      var usernames = __.pluck(scope.users, 'username');
+      if(scope.user.username && !__.contains(usernames, scope.user.username)){
+        var user = {
+          username : scope.user.username,
+          status : true
+        };
+        Socket.emit( 'add user', user);
+      }
+    };
+
+    scope.removeUser = function () {
+      var usernames = __.pluck(scope.users, 'username');
+      if(scope.user.username && !__.contains(usernames, scope.user.username)){
+        var user = {
+          username : scope.user.username,
+          status : false
+        };
+        Socket.emit( 'remove user', user);
+      }
+    };
 
     scope.sendMessage = function () {
       var params = {
-        username: 'Username',
+        username: scope.user.username,
         date: new Date(),
-        message: scope.message
+        message: scope.message,
+        type: 'normal'
       };
-
       Socket.emit( 'send message', params );
     };
   }
